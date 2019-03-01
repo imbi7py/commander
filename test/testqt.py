c3af2d9bbb4feffdd7f2d1e488fa5b7339448170
cjl <cuijialiang@pku.edu.cn>
2019-03-01 20:24:21 +0800

    add testqt

---
+++ test/testqt.py
@@ -0,0 +1,43 @@
+import logging
+
+qgispath = 'C:/Program Files/QGIS 3.6'
+pyqt_plugins = qgispath + '/apps/Qt5/plugins'
+qgis_pypath = qgispath + '/apps/qgis/python'
+qgis_python_sitepkgspath = qgispath + '/apps/Python37/Lib/site-packages'
+
+def load_qgis():
+    try:
+        import sys
+        sys.path = [qgis_pypath, qgis_python_sitepkgspath] + sys.path
+        import PyQt5
+        import qgis.core
+        import qgis.gui
+        return True
+    except Exception as e:
+        logging.error('load qgis failed')
+        logging.exception(e)
+        return False
+
+import sys
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+
+
+app = QApplication(sys.argv)
+
+class DemoImpl(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        loadUi('main_window.ui', self)
+    
+    @pyqtSlot()
+    def on_testButton_clicked(self):
+        for s in "This is a demo".split(" "):
+            self.testList.addItem(s)
+
+
+form = DemoImpl()
+form.show()
+sys.exit(app.exec_())
