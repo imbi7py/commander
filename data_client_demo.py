79dead7e743654c1cb7a256b2ee904dce6173ac8
cuijialiang@byted <cuijialiang@bytedance.com>
2019-03-18 16:28:43 +0800

    can send image

--- data_client_demo.py
+++ data_client_demo.py
@@ -1,21 +1,36 @@
-import sys
-import socket, config
+# coding:utf-8
+import sys, socket, config, json
+import img_utils
 
 cfg = config.load_config()
 
-
-def client(ip, port, message):
+def send_data_to_ip_port(ip, port, data_):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ip, port))
-        sock.sendall(bytes(message, 'ascii'))
+        data_ = data_.encode('utf8')
+        sock.sendall(data_)
         response = str(sock.recv(1024), 'ascii')
-        print("Received: {}".format(response))
+        return response
+
+def send_string(ip, port, message):
+    data_ = json.dumps({'type': 'str', 'data': message})
+    send_data_to_ip_port(ip, port, data_)
+    print ('send string success')
+    return 0
+
+def send_img(ip, port, pil_img):
+    data_ = json.dumps({'type': 'img', 'data': img_utils.img_to_str(pil_img)})
+    send_data_to_ip_port(ip, port, data_)
+    print ('send img success')
+
 
 def main():
+    from PIL import Image
     ip, port = cfg['data_server_ip'], int(cfg['data_server_port'])
-    client(ip, port, "Hello World 1")
-    client(ip, port, "Hello World 2")
-    client(ip, port, "Hello World 3")
+    send_string(ip, port, "Hello World 1")
+    send_string(ip, port, "Hello World 2")
+    send_img(ip, port, Image.open('pics/emojis/0.png'))
+    send_img(ip, port, Image.open('pics/emojis/1.png'))
 
 
 if __name__ == '__main__':
--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,11 @@
+# coding:utf-8
+import json
 class DataHandler():
     def __init__(self, rc):
         self.rc = rc
-    
+
     def process_received_data(self, data):
-        self.rc.main_window.add_item_to_list(data)
-        print ('[data handler]recv  %s' % data)
+        data = json.loads(data)
+        #self.rc.main_window.add_item_to_list(data)
+        print ('[data handler]recv a  %s' % data['type'])
         return '0'
