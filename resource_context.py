bfa8c022e459b33a0a917c2cbdf18485c13c85f0
cuijialiang@byted <cuijialiang@bytedance.com>
2019-03-31 21:05:35 +0800

    fix quickview

--- resource_context.py
+++ resource_context.py
@@ -1,5 +1,5 @@
 import config
-#import data_server, data_handler, quickview_store
+import data_server, data_handler, quickview_store
 import mission_manager
 
 class ResourceContext():
@@ -13,6 +13,6 @@ class ResourceContext():
         self.main_window = main_window
         self.cfg = config.get_config()
         self.mission_manager = mission_manager.MissionManager(self)
-        #self.data_socket_server = data_server.DataServer(self)
-        #self.data_handler = data_handler.DataHandler(self)
-        #self.quickview_store = quickview_store.QuickviewStore(self)
+        self.data_socket_server = data_server.DataServer(self)
+        self.data_handler = data_handler.DataHandler(self)
+        self.quickview_store = quickview_store.QuickviewStore(self)
