34bdcffec942facccde12b26c5ed2d80ef3f67d4
cjl <cuijialiang@pku.edu.cn>
2019-06-22 22:51:20 +0800

    fix

--- mission_simulate.py
+++ mission_simulate.py
@@ -1,4 +1,4 @@
-import PyQt5.QtCore, time, threading, qgis.gui, qgis.core, math
+import PyQt5.QtCore, time, threading, qgis.gui, qgis.core, math, logging
 
 class Point_Simulation():
     def __init__(self, rc, start_position):
@@ -46,27 +46,30 @@ class Polyline_Simulation():
         self.simulate_thread.start()
     
     def run(self):
-        def get_direction_to_east(p1, p2):
-            delta_x = p2[0] - p1[0]
-            delta_y = p2[1] - p1[1]
-            direction = 90.
-            if abs(delta_x) < 0.000001:
-                if delta_y > 0:
-                    direction = 90.
+        try:
+            def get_direction_to_east(p1, p2):
+                delta_x = p2[0] - p1[0]
+                delta_y = p2[1] - p1[1]
+                direction = 90.
+                if abs(delta_x) < 0.000001:
+                    if delta_y > 0:
+                        direction = 90.
+                    else:
+                        direction = -90
                 else:
-                    direction = -90
-            else:
-                direction = math.atan(delta_y / delta_x) / math.pi * 180.
-                if delta_x < 0:
-                    direction += 180.
-            return direction
+                    direction = math.atan(delta_y / delta_x) / math.pi * 180.
+                    if delta_x < 0:
+                        direction += 180.
+                return direction
 
-        for i in range(len(self.polyline)):
-            direction = 0.
-            if i == len(self.polyline) - 1:
-                direction = get_direction_to_east(self.polyline[i-1], self.polyline[i])
-            else:
-                direction = get_direction_to_east(self.polyline[i], self.polyline[i+1])
-            self.point_simu.move_to(self.polyline[i], direction)
-            time.sleep(0.2)
-        self.point_simu.hide()
+            for i in range(len(self.polyline)):
+                direction = 0.
+                if i == len(self.polyline) - 1:
+                    direction = get_direction_to_east(self.polyline[i-1], self.polyline[i])
+                else:
+                    direction = get_direction_to_east(self.polyline[i], self.polyline[i+1])
+                self.point_simu.move_to(self.polyline[i], direction)
+                time.sleep(0.2)
+            self.point_simu.hide()
+        except Exception as e:
+            logging.exception(e)
