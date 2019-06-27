aeba2c961c1da9584c8a663f94c95eb652c64882
cjl <cuijialiang@pku.edu.cn>
2019-06-27 11:43:37 +0800

    update mid term menu item

--- mid_term_experiment.py
+++ mid_term_experiment.py
@@ -8,14 +8,40 @@ def create_mid_term_experiment(rc):
     mm.add_area('人、车目标跟踪观测区（区域3）', [(117.3906, 39.5543), (117.3906, 39.5505), (117.4100, 39.5505), (117.4100, 39.5543)])
     mm.add_fly_mission_to_area({
         'area_name': '生态监测区（区域1）',
-        'mission_name': '植被覆盖提取生态观测任务',
+        'mission_name': '植被覆盖提取生态观测任务_可见光',
         'aerocraft': '猛牛-轻小型固定翼无人机',
-        'cameras': '多光谱相机',
+        'cameras': '光学相机（照片）',
         'ground_resolution_m': 0.1,
         'forward_overlap': 0.8,
         'sideway_overlap': 0.8,
         'fly_direction': 'longest_edge',
         'application': '大尺度生态应用(中期实验)',
         'aerocraft_num': 8,
+        'board_region_name': '翱翔5km圆',
+    })
+    mm.add_fly_mission_to_area({
+        'area_name': '水域观测区（区域2）',
+        'mission_name': '水域提取_Sar',
+        'aerocraft': '猛牛-轻小型固定翼无人机',
+        'cameras': 'minisar',
+        'ground_resolution_m': 0.1,
+        'forward_overlap': None,
+        'sideway_overlap': 0.2,
+        'fly_direction': 'longest_edge',
+        'application': '中尺度洪涝应用(中期实验)',
+        'aerocraft_num': 1,
+        'board_region_name': '翱翔5km圆',
+    })
+    mm.add_fly_mission_to_area({
+        'area_name': '人、车目标跟踪观测区（区域3）',
+        'mission_name': '人、车目标跟踪',
+        'aerocraft': '猛牛-轻小型固定翼无人机',
+        'cameras': '轻型双波段相机(红外)',
+        'ground_resolution_m': 0.1,
+        'forward_overlap': None,
+        'sideway_overlap': 0.2,
+        'fly_direction': 'longest_edge',
+        'application': '小尺度应急应用(中期实验)',
+        'aerocraft_num': 1,
         'board_region_name': '翱翔5km圆',
     })
